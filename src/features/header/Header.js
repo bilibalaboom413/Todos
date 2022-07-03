e4832f081ca61977cde26e85719d23b14c03c41d
Hao Chen <hche8093@uni.sydney.edu.au>
2022-07-03 12:03:36 +0800

    Add header status

--- src/features/header/Header.js
+++ src/features/header/Header.js
@@ -6,20 +6,27 @@ import { saveNewTodo } from '../todos/todosSlice'
 
 const Header = () => {
   const [text, setText] = useState('')
+  const [status, setStatus] = useState('idle')
   const dispatch = useDispatch()
 
   const handleChange = (e) => setText(e.target.value)
 
-  const handleKeyDown = (e) => {
+  const handleKeyDown = async (e) => {
     const trimmedText = e.target.value.trim()
     // e.which === 13
     if (e.key === 'Enter' && trimmedText) {
       // dispatch({ type: 'todos/todoAdded', payload: trimmedText })
       // const saveNewTodoThunk = saveNewTodo(trimmedText)
-      dispatch(saveNewTodo(trimmedText))
+      setStatus('loading')
+      await dispatch(saveNewTodo(trimmedText))
       setText('')
+      setStatus('idle')
     }
   }
+
+  let isLoading = status === 'loading'
+  let placeholder = isLoading ? '' : 'What needs to be done?'
+  let loader = isLoading ? <div className="loader" /> : null
 
   return (
     // <header className="header">
@@ -30,14 +37,19 @@ const Header = () => {
     //     onChange={handleChange}
     //   />
     // </header>
-    <input
-      type="text"
-      placeholder="What needs to be done?"
-      autoFocus={true}
-      value={text}
-      onChange={handleChange}
-      onKeyDown={handleKeyDown}
-    />
+    <header className="header">
+      <input
+        className="new-todo"
+        type="text"
+        placeholder={placeholder}
+        autoFocus={true}
+        value={text}
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
+        disabled={isLoading}
+      />
+      {loader}
+    </header>
   )
 }
 
